c7156c1f9aa5dc2fc48e807075964da03bbfd4fd
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-15 05:53:53 -0700

    Insert Multiple records

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,5 @@
--- Use the alx_book_store database
 USE alx_book_store;
 
--- Insert multiple rows into the 'customers' table
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
