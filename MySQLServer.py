feeb5529c355716e9f77526d7a0d2ab70e7322cd
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-15 05:03:29 -0700

    Python script that creates database

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ def create_db():
 
             cursor.execute(f"CREATE DATABASE IF NOT EXISTS alx_book_store;")
             print(f"Database alx_book_store created successfully!")
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:
