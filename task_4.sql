8705a5d92d074f9154e7e57b873037149fe75116
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-15 05:34:07 -0700

    Print full description pf the table Books

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 -- Get the full description of the 'books' table from the 'alx_book_store' database
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
 AND TABLE_NAME = 'books';
 
