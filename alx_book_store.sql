6c68f99a92a193f4f95bd05732ded7d843185f3d
solianadaniel1 <solianadaniel22@gmail.com>
2024-10-15 01:30:04 -0700

    Bookstore Database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -11,45 +11,34 @@ DROP TABLE IF EXISTS Books;
 DROP TABLE IF EXISTS Customers;
 DROP TABLE IF EXISTS Authors;
 
--- Create the Authors table
-CREATE TABLE Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
+CREATE DATABASE alx_book_store;
+--Use current database
+USE alx_book_store;
 
--- Create the Books table
-CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE CASCADE
+CREATE TABLE Books(
+book_id INT PRIMARY_KEY,
+title VARCHAR(130),
+price DOUBLE,
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-
--- Create the Customers table
-CREATE TABLE Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL UNIQUE,
-    address TEXT
+CREATE TABLE Authors(
+author_id INT PRIMARY_KEY,
+author_name VARCHAR(215)
 );
-
--- Create the Orders table
-CREATE TABLE Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
+CREATE TABLE Customers(
+customer_id INT PRIMARY_KEY,
+customer_name VARCHAR(215),
+address TEXT
 );
-
--- Create the Order_Details table
-CREATE TABLE Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
+CREATE TABLE Orders(
+order_id INT PRIMARY_KEY,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-
+CREATE TABLE Order_Details(
+orderdetail_id INT PRIMARY KEY,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
+quality DOUBLE
+);
